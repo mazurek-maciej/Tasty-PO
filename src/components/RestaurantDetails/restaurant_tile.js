1d6a80f8b0699191088d110ec80ad9e488b13f92
maciejmazurek <maciej.mazurek.prv@gmail.com>
2018-12-03 10:24:46 +0100

    added map with marker

--- src/components/RestaurantDetails/restaurant_tile.js
+++ src/components/RestaurantDetails/restaurant_tile.js
@@ -3,26 +3,80 @@ import img from "../../images/img1.jpeg";
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
-import Map from '../map'
 import { compose } from 'redux'
 import { Redirect } from 'react-router-dom'
+import styled from 'styled-components'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import icon from 'leaflet/dist/images/marker-icon.png'
+import shadowIcon from 'leaflet/dist/images/marker-shadow.png'
+
+const MapContainer = styled.div `
+    .leaflet-pane .leaflet-marker-pane {
+        background-color: red
+    }
+`;
+const MarkerWraper = styled.div `
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: flex-end
+`;
+
+let myIcon = L.icon({
+    iconUrl: icon,
+    iconSize: [30, 50],
+    iconAnchor: [15, 50],
+    popupAnchor: [0, -48],
+    shadowUrl: shadowIcon,
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+});
 
 class RestaurantTile extends Component {
+    state = {
+        lat: 50.674577,
+        lng: 17.918693,
+        zoom: 13,
+      }
+    componentDidUpdate({ markerPosition }) {
+    // check if position has changed 
+    if (this.props.markerPosition !== markerPosition) {
+        this.marker.setLatLng(this.props.markerPosition);
+    }
+    }
     render() {
         const { auth, restaurantName } = this.props;
+        const position = [this.state.lat, this.state.lng]
         if (!auth.uid) return <Redirect to='/signin' />;
 
         return (
             <section className="section">
                 <div className="container">
-                <Map 
-                    isMarkerShown
-                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC8hRiRDW-V6iWz81B3ROPh5-ZjtTTEuPQ&v=3.exp&libraries=geometry,drawing,places"
-                    loadingElement={<div style={{ height: `100%` }} />}
-                    containerElement={<div style={{ height: `80vh` }} />}
-                    mapElement={<div style={{ height: `100%` }} />}
-                        
-                    />
+                <MapContainer>
+                    <Map style={{height: '600px'}} center={position} zoom={this.state.zoom}>
+                        <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                        />
+                        <Marker 
+                        position={position}
+                        icon={myIcon}
+                        >
+                        <Popup>
+                        <MarkerWraper>
+                            <h2 className='subtitle'>
+                                Check out this awesome restaurant!
+                            </h2>
+                            <div>
+                                <Link className='button is-info' to='/restaurant/kfc'>Check</Link>
+                            </div>
+                        </MarkerWraper>
+                        </Popup>
+                        </Marker>
+                    </Map>
+                </MapContainer>
                 </div>
             </section>
         )
