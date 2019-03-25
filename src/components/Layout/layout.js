29ff214dd1aeddd09766e983a7a17c6b06e26fc9
maciejmazurek <maciej.mazurek.prv@gmail.com>
2019-03-25 17:04:33 +0100

    Readme

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {createGlobalStyle, ThemeProvider} from 'styled-components';
-import {theme} from '../../utils/theme';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { theme } from '../../utils/theme';
 
 const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
     font-size: 16px;
-    color: ${({theme}) => theme.colors.$dark};
+    color: ${({ theme }) => theme.colors.$dark};
     font-family: Source Sans Pro;
     *, *::before, *::after {
       box-sizing: border-box;
@@ -15,12 +15,12 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const Layout = ({children}) => (
+const Layout = ({ children }) => (
   <ThemeProvider theme={theme}>
-    <>
+    <React.Fragment>
       <GlobalStyle />
       {children}
-    </>
+    </React.Fragment>
   </ThemeProvider>
 );
 
